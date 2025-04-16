664c3db0ae0ecc9bcf1e08e8fd5ff076590e3942
t-takamichi <takamichitanaka10@gmail.com>
2025-04-16 22:52:09 +0900

    first commit

---
+++ src/main/java/work/docuper/dockuper_api/DockuperApiApplication.java
@@ -0,0 +1,13 @@
+package work.docuper.dockuper_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockuperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockuperApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/work/docuper/dockuper_api/DockuperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package work.docuper.dockuper_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockuperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
