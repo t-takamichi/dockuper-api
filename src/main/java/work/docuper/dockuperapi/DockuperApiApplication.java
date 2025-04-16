92fe5bc469c06f729333d59d5efa14d8657b4029
t-takamichi <takamichitanaka10@gmail.com>
2025-04-16 22:57:35 +0900

    update:リファクタ　パッケージ名

--- src/main/java/work/docuper/dockuperapi/DockuperApiApplication.java
+++ src/main/java/work/docuper/dockuperapi/DockuperApiApplication.java
@@ -1,4 +1,4 @@
-package work.docuper.dockuper_api;
+package work.docuper.dockuperapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/work/docuper/dockuperapi/DockuperApiApplicationTests.java
+++ src/test/java/work/docuper/dockuperapi/DockuperApiApplicationTests.java
@@ -1,4 +1,4 @@
-package work.docuper.dockuper_api;
+package work.docuper.dockuperapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
